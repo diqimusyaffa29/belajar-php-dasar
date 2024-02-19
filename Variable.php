5fc570eb9c0d547b8632194b19ab67cd846a8bbd
diqimusyaffa29 <diqimusyaffa4@gmail.com>
2024-02-19 20:17:00 +0800

    Belajar Variable

---
+++ Variable.php
@@ -0,0 +1,13 @@
+<?php
+
+$name = "Muhammad Shiddiqi Musyaffa' \n";
+$age = 23;
+
+echo($name);
+echo($age);
+
+// Variable Variables
+$nama = "Diqi";
+$$nama = "Mantap"; //Value sebelumnya dari variable $nama ditimpa menjadi "mantap" ke dalam sebuah variable $$nama
+
+echo $$nama;
