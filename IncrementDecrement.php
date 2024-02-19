f2b48f366c7570c5e9da2e1942e76fb3ab2467c8
diqimusyaffa29 <diqimusyaffa4@gmail.com>
2024-02-19 21:06:04 +0800

    Belajar increment dan decrement

---
+++ IncrementDecrement.php
@@ -0,0 +1,9 @@
+<?php
+
+$a = 1;
+$a++; //maka value dalam variabel a akan bertambah 1
+$a--; //maka value dalam variabel a akan berkurang 1
+$a++; //maka value dalam variabel a akan bertambah 1
+
+// echo($a);
+var_dump($a);
