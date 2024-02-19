e5caa3bfaab68364e1458a11f10b88da225943be
diqimusyaffa29 <diqimusyaffa4@gmail.com>
2024-02-19 20:57:29 +0800

    Belajar Operator Perbandingan

---
+++ OperatorPerbandingan.php
@@ -0,0 +1,6 @@
+<?php
+
+var_dump("10" == 10); //hasilnya akan true karena sama sama 10
+var_dump("10" === 10); //hasilnya akan false karena tipe datanya berbeda
+var_dump(10 >= 10); //Hasilnya akan true
+var_dump(10 > 9); //hasilnya akan true
