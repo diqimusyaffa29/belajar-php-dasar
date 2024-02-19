80c316ded0724586fd702aab2126c09f4d42b376
diqimusyaffa29 <diqimusyaffa4@gmail.com>
2024-02-19 20:09:57 +0800

    Belajar Tipe Data Boolean dan String

---
+++ TipeDataString.php
@@ -0,0 +1,12 @@
+<?php
+
+echo"Nama: ";
+echo"Muhammad Shiddiqi Musyaffa";
+echo "\n";
+echo "Ganteng";
+
+echo <<<FAR
+Ini adlaah contoh string yang sangat panjang
+dan juga gak perlu ngetik enter
+secara manual, "bisa quote" juga.
+FAR;
