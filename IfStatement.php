c70afc9fa113a36cae8607285aafd612fb299f65
diqimusyaffa29 <diqimusyaffa4@gmail.com>
2024-02-20 21:02:26 +0800

    Learning If Statement

---
+++ IfStatement.php
@@ -0,0 +1,16 @@
+<?php
+
+$nilai = 0;
+$absen = 0;
+
+if ($nilai >= 80 && $absen >= 80) {
+    echo "Nilai Anda A". PHP_EOL;
+} elseif($nilai >= 70 && $absen >= 70){
+    echo "Nilai Anda B". PHP_EOL;
+} elseif($nilai >= 60 && $absen >= 60){
+    echo "Nilai Anda C". PHP_EOL;
+} elseif($nilai >= 50 && $absen >= 50){
+    echo "Nilai Anda D". PHP_EOL;
+} else{
+    echo "BODOH". PHP_EOL;
+}
