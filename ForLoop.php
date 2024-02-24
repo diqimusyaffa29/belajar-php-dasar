09c662c32027984e44014206bee5f3e047a2c5d6
diqimusyaffa29 <diqimusyaffa29@gmail.com>
2024-02-24 09:31:27 +0800

    Learning ForLoop

---
+++ ForLoop.php
@@ -0,0 +1,16 @@
+<?php
+
+// for ( ; ;){
+//     echo "INFINITYYY" . PHP_EOL;
+// }
+
+
+$counter = 1;
+
+for ($counter; $counter <= 10; $counter++) {
+    echo "Ini adalah ForLoop ke-{$counter}" . PHP_EOL;
+}
+
+for ($counter = 10; $counter >= 1; $counter--) {
+    echo "Ini adalah ForLoop ke-{$counter}" . PHP_EOL;
+}
