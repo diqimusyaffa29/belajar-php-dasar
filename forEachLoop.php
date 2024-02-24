a3803d866e03e899b29fc4a1ecc6dad1f813033a
diqimusyaffa29 <diqimusyaffa4@gmail.com>
2024-02-24 13:33:15 +0800

    Adding Associative Array

--- forEachLoop.php
+++ forEachLoop.php
@@ -2,6 +2,17 @@
 
 $names = ["Muhammad","Shiddiqi", "Musyaffa'"];
 
-foreach ($names as $r){
-    echo $r . PHP_EOL;
+//foreach ($names as $r){
+//    echo $r . PHP_EOL;
+//}
+
+// Foreach dengan menggunakan associative array
+$names2 = [
+    "firstName" => "Muhammad",
+    "middleName" => "Shiddiqi",
+    "lastName" => "Musyaffa'"
+];
+
+foreach ($names2 as $q => $value){
+    echo $value. PHP_EOL;
 }
