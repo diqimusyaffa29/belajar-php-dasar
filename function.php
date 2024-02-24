d6105004ff2a50992440a50f5d84772b3be2b71f
diqimusyaffa29 <diqimusyaffa4@gmail.com>
2024-02-24 13:45:53 +0800

    Learning function part1

---
+++ function.php
@@ -0,0 +1,17 @@
+<?php
+
+function sayHello(){
+    echo "Hello Function". PHP_EOL;
+}
+
+//sayHello();
+
+$buat = false;
+
+if ($buat){
+    function hi()
+    {
+        echo "hi" .PHP_EOL;
+    }
+}
+hi();
