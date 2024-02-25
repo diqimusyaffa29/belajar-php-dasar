275e79dfc9421c046c7767c6e8ede16c90295ced
diqimusyaffa29 <diqimusyaffa4@gmail.com>
2024-02-25 13:24:14 +0800

    Adding Return Type Declarations for Function

--- functionReturnValue.php
+++ functionReturnValue.php
@@ -1,11 +1,16 @@
 <?php
 
 function sum (int $a, int $b){
-    return $a + $b;
+    return $a + $b . PHP_EOL;
 }
 
 $r = sum(10,10);
-echo $r;
+echo $r ;
 
-$r = sum(10,10);
-echo $r;
+//Return Type Declarations
+
+function sum2(int $a, int $b): int{
+    return $a * $b . PHP_EOL;
+}
+
+echo sum2(2,5);
