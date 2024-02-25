9ed1f3e20a07788a12539e22b634c25f2ac3b1a9
diqimusyaffa29 <diqimusyaffa4@gmail.com>
2024-02-25 13:31:31 +0800

    Learning Variabel Function

---
+++ variableFunction.php
@@ -0,0 +1,10 @@
+<?php
+function foo (){
+    echo "FOO" . PHP_EOL;
+}
+function bar (){
+    echo "BAR" . PHP_EOL;
+}
+
+$functionName = "foo"; //me re assign function dengan nama foo ke dalam sebuah variable
+$functionName();
