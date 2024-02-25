b0ca8485ec6f4a84a2cb30b1c15f5df7289b6a35
diqimusyaffa29 <diqimusyaffa4@gmail.com>
2024-02-25 14:44:14 +0800

    Learning String Function

---
+++ StringFunction.php
@@ -0,0 +1,8 @@
+<?php
+
+var_dump(join(",", [10,11,12,13,14,15]));
+var_dump(explode(" ", "Diqi Musyaffa"));
+var_dump(strtolower("DIQI"));
+var_dump(strtoupper("diqi"));
+var_dump(trim("                       1                     2                      4            6 ")); //hanya  menghilangkan whitespace di awal dan di akhir saja
+var_dump(substr("Diqi Musyaffa", 0, 4));
