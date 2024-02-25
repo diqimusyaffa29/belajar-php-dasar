f9ec30f9ca0c4482db392d40c2138c3b9678d402
diqimusyaffa29 <diqimusyaffa4@gmail.com>
2024-02-25 14:13:37 +0800

    Learning Arrow Function PHP

---
+++ ArrowFunction.php
@@ -0,0 +1,10 @@
+<?php
+
+$depan = "Diqi";
+$belakang  = "Musyaffa'";
+
+$sayHello = fn() => "Hello $depan $belakang";
+
+echo $sayHello();
+
+//Arrow Function pada php ini adalah bentuk sederhana dari anonymous function
